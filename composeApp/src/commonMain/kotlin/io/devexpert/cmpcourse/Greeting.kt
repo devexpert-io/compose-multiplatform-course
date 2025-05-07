710216a57d8a16658b1c3368294722837c3f4e7b
antoniolg <antonio@devexperto.com>
2025-05-07 13:10:33 +0200

    Cleanup boilerplate code

--- composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.devexpert.cmpcourse
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.devexpert.cmpcourse
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
