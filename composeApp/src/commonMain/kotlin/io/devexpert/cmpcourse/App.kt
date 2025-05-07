25632d5f6ced22f3f2d3f54f3677f9cf7c158a52
antoniolg <antonio@devexperto.com>
2025-05-07 13:18:53 +0200

    Add clickable Text with padding and background

--- composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
@@ -1,14 +1,17 @@
 package io.devexpert.cmpcourse
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -16,11 +19,13 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         Box(Modifier.statusBarsPadding().background(Color.White)) {
-            Button(onClick = {
-                println("Button clicked!")
-            }) {
-                Text("Click me!")
-            }
+            Text(
+                text = "Click me!",
+                modifier = Modifier
+                    .clickable { println("Clicked") }
+                    .background(Color.Magenta, shape = RoundedCornerShape(8.dp))
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            )
         }
     }
 }
