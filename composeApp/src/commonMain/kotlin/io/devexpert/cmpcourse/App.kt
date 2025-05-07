0e1f74ec412c59aa28d507471a5ff66976e0f331
antoniolg <antonio@devexperto.com>
2025-05-07 13:10:43 +0200

    Initial sample for the application UI

--- composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
@@ -1,37 +1,25 @@
 package io.devexpert.cmpcourse
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import composemultiplatformcourse.composeapp.generated.resources.Res
-import composemultiplatformcourse.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth().statusBarsPadding(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
+        Box(Modifier.statusBarsPadding().background(Color.White)) {
+            Button(onClick = {
+                println("Button clicked!")
+            }) {
                 Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
             }
         }
     }
