d7c9ceb08e184a9f5c1379e05837384eac63807a
antoniolg <antonio@devexperto.com>
2025-05-07 13:33:30 +0200

    Replace Box with Column layout and add multiple buttons

--- composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
@@ -2,13 +2,19 @@ package io.devexpert.cmpcourse
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,8 +24,17 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Box(Modifier.statusBarsPadding().background(Color.White)) {
+        Column (
+            modifier = Modifier
+                .statusBarsPadding()
+                .background(Color.Yellow)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+        ) {
             MyButton("Click me!")
+            MyButton("Click me too!")
+            MyButton("Click me again!")
         }
     }
 }
