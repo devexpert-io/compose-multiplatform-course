8d9e564a5d0c6d0a2e73fdec601fd247ff369380
antoniolg <antonio@devexperto.com>
2025-05-07 13:45:36 +0200

    Build Login form

--- composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/cmpcourse/App.kt
@@ -1,18 +1,15 @@
 package io.devexpert.cmpcourse
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -20,27 +17,22 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Column (
-            modifier = Modifier
-                .statusBarsPadding()
-                .background(Color.Yellow)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
         ) {
-            MyButton("Click me!", modifier = Modifier.weight(1f))
-            MyButton("Click me too!", modifier = Modifier.weight(1f))
-            MyButton("Click me again!", modifier = Modifier.weight(2f))
+            OutlinedTextField(
+                value = "user",
+                onValueChange = {}
+            )
+            OutlinedTextField(
+                value = "password",
+                onValueChange = {}
+            )
+            Button(onClick = {}) {
+                Text("Login")
+            }
         }
     }
-}
-
-@Composable
-fun MyButton(text: String, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
-        modifier = modifier
-            .clickable { println("Clicked") }
-            .background(Color.Magenta, shape = RoundedCornerShape(8.dp))
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-    )
 }
